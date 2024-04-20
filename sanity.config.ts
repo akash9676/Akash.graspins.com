c4a1af7bb935df76e887f66db000bb17daaf97a2
akash9676 <akashnaidu545@gmail.com>
2024-04-20 12:26:19 +0530

    confilct

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,8 +6,8 @@ import { codeInput } from "@sanity/code-input";
 import { projectId, dataset } from "./lib/env.api";
 
 export default defineConfig({
-  name: "victoreke",
-  title: "victoreke.com",
+  name: "Akash",
+  title: "Akash Gundapuneni",
   basePath: "/studio",
   projectId,
   dataset,
